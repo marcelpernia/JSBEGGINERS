961831852801d401aea6df1ac8600849bc3f0626
Marcel Pernia <marcelpm1987@gmail.com>
2022-11-20 23:12:16 -0500

    calculator refactoring

--- 6-Calculator/scripts.js
+++ 6-Calculator/scripts.js
@@ -4,27 +4,31 @@ const btn = document.querySelectorAll('.btn');
 
 calculator.addEventListener('click', ({target: {className, textContent}}) => {
     let displayCurrentValue = display.textContent
-    
-    if(className === 'btn') {
-        displayCurrentValue === '0' ? display.textContent = textContent : display.textContent += textContent
-    }
-    
-    if(className === 'operator') {
-        displayCurrentValue !== '0' && !displayCurrentValue.endsWith(textContent) && (display.textContent += textContent)
-    }
 
-    if(className === 'dot') {
-        display.textContent += textContent
-    }
-
-    if(className === 'clear') {
-        display.textContent = '0';
-    }
-    
-    if(className === 'equal') {
-        let result = displayCurrentValue.replaceAll('×', '*').replaceAll('÷', '/');
-        result = eval(result)
-        result = result.toString().length > 10 ? result.toFixed(10) : result
-        display.textContent = result
+    switch(className) {
+        case 'btn':
+            displayCurrentValue === '0' ? display.textContent = textContent : display.textContent += textContent    
+            break;
+        case 'operator':
+            displayCurrentValue !== '0' && !displayCurrentValue.endsWith(textContent) && (display.textContent += textContent)
+            break;
+        case 'dot':
+            !displayCurrentValue.endsWith(textContent) && (display.textContent += textContent)
+            break;
+        case 'clear':
+            display.textContent = '0'
+            break
+        case 'plusminus':
+            Math.sign(displayCurrentValue) === 1 ? display.textContent = 0 - displayCurrentValue : display.textContent = Math.abs(displayCurrentValue);
+            break;
+        case 'percent':
+            display.textContent = displayCurrentValue / 100
+            break;
+        case 'equal':
+            let result = displayCurrentValue.replaceAll('×', '*').replaceAll('÷', '/');
+            result = eval(result)
+            result = result.toString().length > 10 ? result.toFixed(10) : result
+            display.textContent = result
+            break;
     }
 })
